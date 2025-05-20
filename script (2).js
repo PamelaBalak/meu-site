93325388e61708415ecac415c635bd3f6a8689b9
PamelaBalak <pamelacristiniferreira@gmail.com>
2025-05-19 21:30:52 -0300

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.textContent = "O futuro pertence a aqueles que acreditam na beleza dos seus sonhos.";
+}
